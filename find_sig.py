6806e556b246686d79eacf28728fc7f983fc4127
arkady <arkady>
2021-03-11 21:36:46 +0200

    comment

--- find_sig.py
+++ find_sig.py
@@ -8,7 +8,7 @@ Cutting corners: I assume SSD, this script will run slow on HDD
 1TB disk will take ~10 minutes
 
 For HDD I need to map the block device, read sequentially, search, 
-link the found blocks to the filepath
+link the found block to the filepath
 '''
 
 import os
